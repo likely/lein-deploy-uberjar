030f3bdde53abe3a35ea96c2eb25855d725fe779
Bob Cotton <bob.cotton@rallydev.com>
2013-02-13 09:16:42 -0700

    Tracking leinengen 2.0.0 deploy task

--- src/leiningen/deploy_uberjar.clj
+++ src/leiningen/deploy_uberjar.clj
@@ -1,13 +1,15 @@
 (ns leiningen.deploy-uberjar
   "Build and deploy standalone jar to remote repository.
-
-This code blatantly ripped from lein's standard deploy task."
-  (:require [leiningen.core.main :as main]
-            [leiningen.uberjar :as uberjar]
-            [leiningen.pom :as pom]
+   This code blatantly ripped from lein's standard deploy task"
+  (:require [cemerick.pomegranate.aether :as aether]
             [leiningen.core.classpath :as classpath]
-            [cemerick.pomegranate.aether :as aether]
-            [clojure.java.io :as io]))
+            [leiningen.core.main :as main]
+            [leiningen.core.eval :as eval]
+            [leiningen.core.user :as user]
+            [clojure.java.io :as io]
+            [leiningen.pom :as pom]
+            [leiningen.jar :as jar]
+            [leiningen.uberjar :as uberjar]))
 
 (defn- abort-message [message]
   (cond (re-find #"Return code is 405" message)
@@ -31,44 +33,67 @@ This code blatantly ripped from lein's standard deploy task."
                                     (into-array ["Password: "]))]
         [id (assoc settings :username username :password password)]))))
 
+(defn repo-for [project name]
+  (let [[settings] (for [[id settings] (concat (:deploy-repositories project)
+                                               (:repositories project)
+                                               [[name {:url name}]])
+                         :when (= id name)] settings)]
+    (-> [name settings]
+        (classpath/add-repo-auth)
+        (add-auth-interactively))))
+
+(defn sign [file]
+  (let [exit (binding [*out* (java.io.StringWriter.)]
+               (eval/sh (user/gpg-program) "--yes" "-ab" file))]
+    (when-not (zero? exit)
+      (main/abort "Could not sign" file))
+    (str file ".asc")))
+
+(defn signatures-for [jar-file pom-file coords]
+  {(into coords [:extension "jar.asc"]) (sign jar-file)
+   (into coords [:extension "pom.asc"]) (sign pom-file)})
+
+(defn files-for [project repo]
+  (let [coords [(symbol (:group project) (:name project)) (:version project)]
+        jar-file (uberjar/uberjar project)
+        pom-file (pom/pom project)]
+    (merge {(into coords [:extension "jar"]) jar-file
+            (into coords [:extension "pom"]) pom-file}
+           (if (and (:sign-releases (second repo) true)
+                    (not (.endsWith (:version project) "-SNAPSHOT")))
+             (signatures-for jar-file pom-file coords)))))
+
+(defn warn-missing-metadata [project]
+  (doseq [key [:description :license :url]]
+    (when (or (nil? (project key)) (re-find #"FIXME" (str (project key))))
+      (main/info "WARNING: please set" key "in project.clj."))))
+
 (defn deploy-uberjar
   "Build uberjar and deploy to remote repository.
 
 The target repository will be looked up in :repositories in project.clj:
 
-  :repositories {\"snapshots\" \"https://internal.repo/snapshots\"
-                 \"releases\" \"https://internal.repo/releases\"
-                 \"alternate\" \"https://other.server/repo\"}
+  :repositories [[\"snapshots\" \"https://internal.repo/snapshots\"]
+                 [\"releases\" \"https://internal.repo/releases\"]
+                 [\"alternate\" \"https://other.server/repo\"]]
 
 If you don't provide a repository name to deploy to, either \"snapshots\" or
 \"releases\" will be used depending on your project's current version. See
 `lein help deploying` under \"Authentication\" for instructions on how to
 configure your credentials so you are not prompted on each deploy."
   ([project repository-name]
-     (doseq [key [:description :license :url]]
-       (when (or (nil? (project key)) (re-find #"FIXME" (str (project key))))
-         (main/info "WARNING: please set" key "in project.clj.")))
-     (let [jarfile (uberjar/uberjar project)
-           pomfile (pom/pom project)
-           ;; can't use merge here due to bug in ordered maps:
-           ;; https://github.com/flatland/ordered/issues/4
-           repo-opts (or (get (:deploy-repositories project) repository-name)
-                         (get (:repositories project) repository-name))
-           repo (cond (not repo-opts) ["inline" {:url repository-name}]
-                      (string? repo-opts) [repository-name {:url repo-opts}]
-                      :else [repository-name repo-opts])
-           repo (classpath/add-repo-auth repo)
-           repo (add-auth-interactively repo)]
-       (main/debug "Deploying to" repo)
-       (try (aether/deploy :coordinates [(symbol (:group project)
-                                                 (:name project))
-                                         (:version project)]
-                           :jar-file (io/file jarfile)
-                           :pom-file (io/file pomfile)
-                           :transfer-listener :stdout
-                           :repository [repo])
-            (catch org.sonatype.aether.deployment.DeploymentException e
-              (main/abort (abort-message (.getMessage e)))))))
+     (warn-missing-metadata project)
+     (let [repo (repo-for project repository-name)
+           files (files-for project repo)]
+       (try
+         (main/debug "Deploying" files "to" repo)
+         (aether/deploy-artifacts :artifacts (keys files)
+                                  :files files
+                                  :transfer-listener :stdout
+                                  :repository [repo])
+         (catch org.sonatype.aether.deployment.DeploymentException e
+           (when main/*debug* (.printStackTrace e))
+           (main/abort (abort-message (.getMessage e)))))))
   ([project]
      (deploy-uberjar project (if (pom/snapshot? project)
                                "snapshots"
